c46989d43ab0bcb11f655edfcb7df8f868f827af
Purushottam Shukla <formalspace@gmail.com>
2018-03-24 15:24:35 +0530

    Login Structure

--- src/components/signup.js
+++ src/components/signup.js
@@ -6,15 +6,57 @@ export default class SingUP extends Component {
     constructor(props){
         super(props);
         this.state = {value:0};
-        this.signup = this.signup.bin
+        this.signup = this.signup.bind(this);
+        this.createDB = this.createDB.bind(this);
     }
     
+    signup(event){
+        var target = event.target
+        console.log("Function name - SignUP")
+        var name = target.name.value;
+        var pass = target.pass.value;
+        const auth = firebase.auth();
+        const promise = auth.createUserWithEmailAndPassword(name,pass);
+        promise.catch(e => console.log(e.message));
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            if(firebaseUser){
+                localStorage.setItem('uid',firebaseUser.uid);
+                this.createDB(firebaseUser.uid);
+                console.log("You are succedfully SignUP")
+            }else{
+                console.log("Not Logged");
+            }
+        })
+        event.preventDefault();
+    }
    
+    createDB(UID){
+        console.log('Function calledd');
+        const DbRef = firebase.database();
+        DbRef.ref('userPre/'+UID).set({
+            1:'noterone',
+        })
+    }
     
     render(){
         return(
             <div>
-            <p>THis is SIngup page</p>
+            <p>THis is Singup page</p>
+            <form onSubmit={this.signup}>
+              <label>
+                Name:
+                <input type="text" name="name" />
+              </label><br/>
+                <label>
+                Pass:
+                <input type="text" name="pass" />
+              </label><br/>
+            <label>
+                Re-Pass:
+                <input type="text" name="repass" />
+              </label><br/>
+              <input type="submit" value="SignUP" />
+            </form>
             </div>
         );
     }
--- src/components/singin.js
+++ src/components/singin.js
@@ -11,8 +11,20 @@ export default class SignIN extends Component {
     
     login(event){
         var target = event.target
-        console.log(target.name.value);
-        console.log("FUC")
+        console.log("Sing In Function");
+        var name = target.name.value;
+        var pass = target.pass.value;
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(name,pass);
+        promise.catch(e => console.log(e.message));
+        firebase.auth().onAuthStateChanged(firebaseUser =>{
+            if(firebaseUser){
+                localStorage.setItem('uid', firebaseUser.uid);
+                console.log("The UID is "+localStorage.getItem('uid'))
+            }else{
+                console.log("Not Logged");
+            }
+        })
         event.preventDefault();
     }
     render(){
